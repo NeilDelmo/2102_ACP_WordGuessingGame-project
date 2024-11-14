dcc9dad34aaf5b20726941cf6bc17131a1b176dc
Neil Delmo <jmardelmo@gmail.com>
2024-11-14 17:02:16 +0800

    Meron ng event 2

--- Events.py
+++ Events.py
@@ -1,13 +1,19 @@
-import Main
+import Main  # Import the main module to access its variables
+
+shop_items = {
+    "Health Potion": 100,
+    "Sword": 150,
+    "Shield": 75
+}
 
 def eventone():
-    print("""You are walking in the town and is on the way to complete a quest that has time limit.
-    You need to reach it in 30 minutes or you wont complete the quest.
-    It was then that you see a little girl crying in the ground.     
+    print("""You are walking in the town and are on the way to complete a quest that has a time limit.
+    You need to reach it in 30 minutes or you won't complete the quest.
+    It was then that you see a little girl crying on the ground.     
     You ask her why she is crying.     
-    She said she lost her pet lizard and she cant find her pet now.     
+    She said she lost her pet lizard and she can't find her pet now.     
     If you decided to help her find and catch her pet lizard you won't""")
-    action = input("Do want to ignore the little girl or help her? Select between Ignore(I)/Help(H)")
+    action = input("Do you want to ignore the little girl or help her? Select between Ignore(I)/Help(H): ")
     if action.upper() == 'I':
         print("You have decided to ignore her and go on your own way.")
         Main.stats['Endurance'] += 2
@@ -16,11 +22,36 @@ def eventone():
     elif action.upper() == 'H':
         print("You decided to help the little girl.")
         Main.stats['Dexterity'] += 1
-        Main.money += 80
-        print("Your Strength has increased. Current stats:", Main.stats)
+        Main.money +=  80
+        print("Your Dexterity has increased. Current stats:", Main.stats)
     else:
         print("Invalid action. Please select either Ignore(I) or Help(H).")
 
-
 def eventtwo():
-    print("""You are walking in a jungle """)
+    print("""'HELP!! SOMEONE PLEASE HELP!!!'
+    You hear a woman screaming in the distance. You rush over to find a broken down carriage and a woman 
+    on her knees surrounded by armed bandits. You can decide if you want to help her or not.
+    """)
+    action = input("Do you want to rescue her or run? Rescue(R)/Run(E): ")
+    if action.upper() == 'R':
+        print("You have decided to rescue the woman. The bandits take out their weapons.")
+        print("'You should have minded your own business!'")
+        if Main.stats['Strength'] and Main.stats['Dexterity'] < 10:
+            Main.life -= 1
+            print("You were overpowered by the bandits! You lost 1 life.")
+            print(Main.status())
+            if Main.life <= 0:
+                print("You have no lives left. Game Over.")
+                return
+        elif Main.stats['Strength'] and Main.stats['Dexterity'] >= 10:
+            print("You have defeated the bandits and have save the woman")
+            Main.money += 50
+            print(f"The woman has given you {Main.money} gold as a reward.")
+            Main.stats['Strength'] += 1
+            print("You have become stronger fighting those bandits and become stronger.")
+            print("You have gained 1 strength")
+            print(Main.status)
+    elif action.upper() == 'E':
+        print("You chose to run away and avoid the confrontation.")
+    else:
+        print("Invalid action. Please select either Rescue(R) or Run(E).")
--- Main.py
+++ Main.py
@@ -1,26 +1,35 @@
 import random
-from Events import eventone
+from Events import eventone, eventtwo
 
+# Player's life
 life = 5
+# Player's job and stats initialization
 jobname = ""
 stats = {}
 money = 100
+inventory = []
+
+def status():
+    lives_display = " ".join(["(@)" for _ in range(life)])  # Create a string of "(@)" for each life
+    print(f"Remaining Lives: {lives_display}")
+    print(f"Current Stats: {stats}")
+    print(f"Money: {money}")
+
+
 def character_job(job):
-    global jobname  # Declare jobname as global to modify it
+    global jobname, stats  # Declare jobname and stats as global to modify them
     if job.upper() == 'W':
         stats = {
             "Strength": 12,
             "Dexterity": 8,
-            "Endurance": 15,
-            "Intelligence": 10
+            "Endurance": 10,
         }
         jobname = "Warrior"
     elif job.upper() == 'A':
         stats = {
-            "Strength": 10,
-            "Dexterity": 15,
+            "Strength": 8,
+            "Dexterity": 12,
             "Endurance": 10,
-            "Intelligence": 12
         }
         jobname = "Archer"
     else:
@@ -28,29 +37,36 @@ def character_job(job):
         return None  # Return None if job is invalid
     return stats  # Return the stats dictionary
 
-def showstat(stats):
-    print(f"Current Stats: {stats}")
-    print(money)
-
 def main_game():
-    # List of events
-    events = [eventone]
-    random.shuffle(events)
-    for event in events:
-        event()
+    global life
+    while life > 0:
+        # List of events
+        events = [eventone, eventtwo]
+        random.shuffle(events)
+        for event in events:
+            event()  # Call the event function
+            if life <= 0:  # Check if life is 0 after each event
+                print("You have no lives left. Returning to the main menu...")
+                return  # Exit the main game loop
 
+while True:
+    print("Welcome to the Adventure Game")
+    name = input("Enter the Character's name: ")
+    job = input("Please select between Warrior(W) or Archer(A): ")
+    character_stats = character_job(job)
 
+    if character_stats:  # Check if character_stats is not None
+        print(f"""You always dreamed of being an adventurer. So as a kid, you trained to become a {jobname}.
+        Now that you have grown up, you feel like you can explore the world.
+        You decided to embark on this adventure to become stronger and become legendary. 
+        Will you succeed in the end though? Only time will tell.""")
 
-print("Welcome to the Adventure Game")
-name = input("Enter the Character's name: ")
-job = input("Please select between Warrior(W) or Archer(A): ")
-character_stats = character_job(job)
+        main_game()  # Start the game
+    else:
+        print("Please restart the game and choose a valid job.")
 
-if character_stats:  # Check if character_stats is not None
-    print(f"""You always dreamed of being an adventurer. So as a kid, you trained to become a {jobname}.
-    Now that you have grown up, you feel like you can explore the world.
-    You decided to embark this adventure to become stronger and become legendary. 
-    Will you succeed in the end though? Only time will tell.""")
-
-else:
-    print("Please restart the game and choose a valid job.")
+    # Ask if the player wants to play again
+    play_again = input("Do you want to play again? (yes/no): ")
+    if play_again.lower() != 'yes':
+        print("Thank you for playing! Goodbye!")
+        break  # Exit the game loop
